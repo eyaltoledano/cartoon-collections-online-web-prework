7fc509b785780eae2bd444e452f509ea2b56a003
Eyal Toledano <eyaltoledano@me.com>
2019-01-28 18:44:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  cheese_types.find do |item|
+  if cheese_types.find do |item|
     array.any?
   else
     nil
